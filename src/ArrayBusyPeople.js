0d267cd0ef6f76e660d40c0041e74f701f008ff9
Lucas Inocencio <113804674+lucas-inocencio@users.noreply.github.com>
2023-02-08 21:31:31 -0300

    Update ArrayBusyPeople.js

--- src/ArrayBusyPeople.js
+++ src/ArrayBusyPeople.js
@@ -6,50 +6,50 @@ class ArrayBusyPeople {
 
     /**
      * The command saves the value and sets the stack empty.
-     * @param {int} value the default value for the each element in the main_array.
-     * @param {int} length the max length of the main_array.
+     * @param {int} value The default value for the each element in the main_array.
+     * @param {int} length The max length of the main_array.
      */
     constructor(value = -1, length = 2**8) {
         this.main_array = new Uint32Array(length);
         this.aux_array = new Uint32Array(length);
         this.stack = [];
-        this.value = value;
+        this.std_value = value;
     }
 
     /**
-     * The command test whether main_array[position] is already defined.
-     * If not, define it. If it was defined, set main_array[position] ← value.
-     * @param {int} position position of the element.
-     * @param {int} value new value for the element.
+     * The command test whether main_array[index] is already defined.
+     * If not, define it. If it was defined, set main_array[index] ← value.
+     * @param {int} index Index of the element.
+     * @param {int} value New value for the element.
      */
-    set(position, value) {
-        if (this.isDefined(position)) this.main_array[position] = value;
+    set(index, value) {
+        if (this.isDefined(index)) this.main_array[index] = value;
         else {
-            this.aux_array[position] = this.stack.length;
-            this.stack.push(position);
-            this.main_array[position] = value;
+            this.aux_array[index] = this.stack.length;
+            this.stack.push(index);
+            this.main_array[index] = value;
         }
     }
 
     /**
-     * The command test whether main_array[position] is defined.
-     * @param {int} position 
+     * The command test whether main_array[index] is defined.
+     * @param {int} index Index of element to be returned. 
      * @returns If so, it returns the element. Otherwise it returns the default value.
      */
-    get(position) {
-        return (this.isDefined(position) ? this.main_array[position] : this.value);
+    get(index) {
+        return (this.isDefined(index) ? this.main_array[index] : this.std_value);
     }
 
     /**
      * The command verify if element are defined.
-     * @param {int} position position of the element.
-     * @returns true if defined, false otherwise.
+     * @param {int} index Index of the element.
+     * @returns True if defined, false otherwise.
      */
-    isDefined(position) {
-        let defined = (position < this.main_array.length) && 
-        (0 <= this.aux_array[position]) && 
-        (this.aux_array[position] <= this.stack.length) && 
-        (this.stack[this.aux_array[position]] == position);
+    isDefined(index) {
+        let defined = (index < this.main_array.length) && 
+        (0 <= this.aux_array[index]) && 
+        (this.aux_array[index] <= this.stack.length) && 
+        (this.stack[this.aux_array[index]] == index);
 
         return defined
     }
