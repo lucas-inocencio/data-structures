2fe1bad9bbd7304ac076cf9d2ce17293c55c16fe
Lucas Inocencio <lucas-inocencio@users.noreply.github.com>
2023-03-12 21:03:27 -0300

    Ok

---
+++ src/SinglyLinkedList/SinglyLinkedList.js
@@ -0,0 +1,64 @@
+const SinglyLinkedList = require("./SinglyLinkedList.js");
+
+/**
+ * Class that implement a singly linked list data structure.
+ */
+class SinglyLinkedList {
+  /**
+   * Initializa an empty singly linked list.
+   */
+  constructor() {
+    this.head = null;
+  }
+
+  /**
+   * 
+   */
+  get() {
+
+  }
+
+  /**
+   * 
+   */
+  set() {
+
+  }
+
+  /**
+   * 
+   */
+  length() {
+
+  }
+
+  /**
+   * 
+   */
+  insert() {
+
+  }
+
+  /**
+   * 
+   */
+  delete() {
+
+  }
+
+  /**
+   * 
+   */
+  find() {
+
+  }
+
+  /**
+   * 
+   */
+  () {
+
+  }
+}
+
+module.exports = SinglyLinkedList;
